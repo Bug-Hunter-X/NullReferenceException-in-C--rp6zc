888c49c08b07a0039d425440a200ddae0504c6cb
BugHunter-X <askzenai@gmail.com>
2024-12-15 03:52:05 +0100

    Add Bug.cs file

---
+++ Bug.cs
@@ -0,0 +1,14 @@
+public class ExampleClass
+{
+    public int MyProperty { get; set; }
+
+    public ExampleClass(int value)
+    {
+        MyProperty = value;
+    }
+
+    public void MyMethod()
+    {
+        // ... some code ...
+    }
+}
